f3d1e6f67db6b4c77474366862bca13b645f5d04
unknown <robson.vieira@sodobrasil.net.br>
2025-06-18 11:52:55 -0300

    upload

--- src/config/firebase/firebase.config.ts
+++ src/config/firebase/firebase.config.ts
@@ -1,9 +1,16 @@
+import fs from 'fs';
 import firebase from 'firebase-admin';
 
-export function setupFirebase(){
-    const serviceAccount = require("./serviceAccountKey.json");
-    
-    firebase.initializeApp({
-      credential: firebase.credential.cert(serviceAccount)
-    });
+export function setupFirebase() {
+  let serviceAccount;
+
+  if (process.env.FIREBASE_CONFIG_JSON) {
+    serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG_JSON);
+  } else {
+    serviceAccount = JSON.parse(fs.readFileSync('./serviceAccountKey.json', 'utf-8'));
+  }
+
+  firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount)
+  });
 }
