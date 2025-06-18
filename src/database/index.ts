87064927fb9e24b3fe7633b73ed251d43f60229d
unknown <robson.vieira@sodobrasil.net.br>
2025-06-18 11:32:57 -0300

    upload

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,14 +1,9 @@
 import Database from "better-sqlite3";
 import path from "path";
-import fs from 'fs';
 
 import { tables } from "./tables";
 
 const dbPath = path.join(__dirname, "../data/database.db");
-
-if (!fs.existsSync(dbPath)) {
-  fs.mkdirSync(dbPath, { recursive: true });
-}
 
 const db = new Database(dbPath);
 
